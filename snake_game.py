190f73f0e35219f6480b83256fc247d4028dc11e
chetanharjani <chetonix@gmail.com>
2024-08-05 17:30:12 +0530

    add more functionality such as collision with tail etc.

--- snake_game.py
+++ snake_game.py
@@ -37,5 +37,11 @@ while game_is_on:
   if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.xcor() < -280:
     game_is_on = False
     scoreboard.game_over()
+
+  #detect collision with tail
+  for segment in snake.segments[1:]:
+    if snake.head.distance(segment) < 10:
+      game_is_on = False
+      scoreboard.game_over()
   
 screen.exitonclick()
